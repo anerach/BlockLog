1b0d62146ad798a055382082b01e5b29899e3a62
Anerach <arno.landuyt@hotmail.com>
2012-04-22 10:59:39 +0200

    Changed interaction logging to use only 1 class

--- src/me/arno/blocklog/listeners/InteractionListener.java
+++ src/me/arno/blocklog/listeners/InteractionListener.java
@@ -2,7 +2,7 @@ package me.arno.blocklog.listeners;
 
 import me.arno.blocklog.BlockLog;
 import me.arno.blocklog.Interaction;
-import me.arno.blocklog.logs.InteractedBlock;
+import me.arno.blocklog.logs.LoggedInteraction;
 
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
@@ -18,29 +18,17 @@ public class InteractionListener extends BlockLogListener {
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		if(!event.isCancelled()) {
 			if(event.getClickedBlock().getType() == Material.WOODEN_DOOR) {
-				InteractedBlock block = new InteractedBlock(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.DOOR);
-				block.push();
-				BlocksLimitReached();
+				plugin.addInteraction(new LoggedInteraction(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.DOOR));
 			} else if(event.getClickedBlock().getType() == Material.TRAP_DOOR) {
-				InteractedBlock block = new InteractedBlock(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.TRAP_DOOR);
-				block.push();
-				BlocksLimitReached();
+				plugin.addInteraction(new LoggedInteraction(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.TRAP_DOOR));
 			} else if(event.getClickedBlock().getType() == Material.CHEST) {
-				InteractedBlock block = new InteractedBlock(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.CHEST);
-				block.push();
-				BlocksLimitReached();
+				plugin.addInteraction(new LoggedInteraction(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.CHEST));
 			} else if(event.getClickedBlock().getType() == Material.DISPENSER) {
-				InteractedBlock block = new InteractedBlock(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.DISPENSER);
-				block.push();
-				BlocksLimitReached();
+				plugin.addInteraction(new LoggedInteraction(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.DISPENSER));
 			} else if(event.getClickedBlock().getType() == Material.STONE_BUTTON) {
-				InteractedBlock block = new InteractedBlock(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.BUTTON);
-				block.push();
-				BlocksLimitReached();
+				plugin.addInteraction(new LoggedInteraction(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.BUTTON));
 			} else if(event.getClickedBlock().getType() == Material.LEVER) {
-				InteractedBlock block = new InteractedBlock(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.LEVER);
-				block.push();
-				BlocksLimitReached();
+				plugin.addInteraction(new LoggedInteraction(plugin, event.getPlayer(), event.getClickedBlock().getLocation(), Interaction.LEVER));
 			}
 		}
 	}

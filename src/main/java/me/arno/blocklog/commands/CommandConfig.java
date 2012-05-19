e1402d846ac99c45bdbf17ab28c9e7795506da2e
Anerach <arno.landuyt@hotmail.com>
2012-05-19 03:22:42 +0200

    Removed old code

--- src/main/java/me/arno/blocklog/commands/CommandConfig.java
+++ src/main/java/me/arno/blocklog/commands/CommandConfig.java
@@ -24,53 +24,50 @@ public class CommandConfig extends BlockLogCommand {
 		String ConfigKey;
 		String ConfigValue;
 		
-		if(player.isOp() || player.hasPermission("blocklog.config")) {
-			if(action.equalsIgnoreCase("help")) {
-				player.sendMessage(ChatColor.DARK_RED + "[BlockLog][Config] " + ChatColor.GOLD + "Help");
-				player.sendMessage(ChatColor.DARK_GREEN + "/blconfig help - Shows this message");
-				player.sendMessage(ChatColor.DARK_GREEN + "/blconfig set <key> <value> - Changes a blocklog config value");
-				player.sendMessage(ChatColor.DARK_GREEN + "/blconfig get <key> - Shows a blocklog config value");
-			} else if(action.equalsIgnoreCase("set")) {
-				if(args.length != 3)
-					return false;
+		if(action.equalsIgnoreCase("help")) {
+			player.sendMessage(ChatColor.DARK_RED + "[BlockLog][Config] " + ChatColor.GOLD + "Help");
+			player.sendMessage(ChatColor.DARK_GREEN + "/bl config help - Shows this message");
+			player.sendMessage(ChatColor.DARK_GREEN + "/bl config set <key> <value> - Changes a blocklog config value");
+			player.sendMessage(ChatColor.DARK_GREEN + "/bl config get <key> - Shows a blocklog config value");
+		} else if(action.equalsIgnoreCase("set")) {
+			if(args.length != 3)
+				return false;
+			
+			ConfigKey = args[1];
+			ConfigValue = args[2];
+			
+			if(getSettingsManager().getConfig().isString(ConfigKey))
+				getSettingsManager().getConfig().set(ConfigKey, ConfigValue);
+			else if(getSettingsManager().getConfig().isInt(ConfigKey))
+				getSettingsManager().getConfig().set(ConfigKey, Integer.parseInt(ConfigValue));
+			else if(getSettingsManager().getConfig().isBoolean(ConfigKey))
+				getSettingsManager().getConfig().set(ConfigKey, Boolean.parseBoolean(ConfigValue));
+			else
+				return false;
+			
+			player.sendMessage(ChatColor.DARK_RED +"[BlockLog][Config] " + ChatColor.GOLD + "Changed value of " + ConfigKey + " to " + ConfigValue);
+			getSettingsManager().saveConfig();
+			getSettingsManager().reloadConfig();
+		} else if(action.equalsIgnoreCase("get")) {
+			if(args.length != 2)
+				return false;
+			
+			ConfigKey = args[1].toString();
+			
+			String Result;
+			if(getSettingsManager().getConfig().isString(ConfigKey))
+				Result = getSettingsManager().getConfig().getString(ConfigKey);
+			else if(getSettingsManager().getConfig().isInt(ConfigKey))
+				Result = Integer.toString(getSettingsManager().getConfig().getInt(ConfigKey));
+			else if(getSettingsManager().getConfig().isBoolean(ConfigKey))
+				Result = Boolean.toString(getSettingsManager().getConfig().getBoolean(ConfigKey));
+			else 
+				return false;
 				
-				ConfigKey = args[1];
-				ConfigValue = args[2];
-				
-				if(getSettingsManager().getConfig().isString(ConfigKey))
-					getSettingsManager().getConfig().set(ConfigKey, ConfigValue);
-				else if(getSettingsManager().getConfig().isInt(ConfigKey))
-					getSettingsManager().getConfig().set(ConfigKey, Integer.parseInt(ConfigValue));
-				else if(getSettingsManager().getConfig().isBoolean(ConfigKey))
-					getSettingsManager().getConfig().set(ConfigKey, Boolean.parseBoolean(ConfigValue));
-				else
-					return false;
-				
-				player.sendMessage(ChatColor.DARK_RED +"[BlockLog][Config] " + ChatColor.GOLD + "Changed value of " + ConfigKey + " to " + ConfigValue);
-				getSettingsManager().saveConfig();
-				getSettingsManager().reloadConfig();
-			} else if(action.equalsIgnoreCase("get")) {
-				if(args.length != 2)
-					return false;
-				
-				ConfigKey = args[1].toString();
-				
-				String Result;
-				if(getSettingsManager().getConfig().isString(ConfigKey))
-					Result = getSettingsManager().getConfig().getString(ConfigKey);
-				else if(getSettingsManager().getConfig().isInt(ConfigKey))
-					Result = Integer.toString(getSettingsManager().getConfig().getInt(ConfigKey));
-				else if(getSettingsManager().getConfig().isBoolean(ConfigKey))
-					Result = Boolean.toString(getSettingsManager().getConfig().getBoolean(ConfigKey));
-				else 
-					return false;
-				
-				player.sendMessage(ChatColor.DARK_RED +"[BlockLog][Config] " + ChatColor.GOLD + "Value of " + ConfigKey + ": " + Result);
-				
-			}
-			return true;
+			player.sendMessage(ChatColor.DARK_RED +"[BlockLog][Config] " + ChatColor.GOLD + "Value of " + ConfigKey + ": " + Result);
+
 		}
-		return false;
+		return true;
 	}
 
 }

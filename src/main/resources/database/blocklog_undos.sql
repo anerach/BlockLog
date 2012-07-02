f0703bd5c13382ecbd4f352d165bdb579f16769d
anerach <arno.landuyt@hotmail.com>
2012-07-02 21:38:34 +0200

    Updated the database tables

--- src/main/resources/database/blocklog_undos.sql
+++ src/main/resources/database/blocklog_undos.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `blocklog_undos` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `rollback` int(11) UNSIGNED NOT NULL,
-  `player` varchar(255) NOT NULL,
-  `date` int(11) UNSIGNED NOT NULL,
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `rollback` INT UNSIGNED NOT NULL,
+  `player` VARCHAR(255) NOT NULL,
+  `date` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   KEY `rollback` (`rollback`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;

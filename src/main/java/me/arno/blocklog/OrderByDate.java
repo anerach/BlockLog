c994005971c3dea43b917d6c088d3d5c588ee1b1
anerach <arno.landuyt@hotmail.com>
2012-08-09 02:35:55 +0200

    Fixed OrderByDate comparator

--- src/main/java/me/arno/blocklog/OrderByDate.java
+++ src/main/java/me/arno/blocklog/OrderByDate.java
@@ -9,9 +9,9 @@ public class OrderByDate implements Comparator<DataEntry> {
 	@Override
 	public int compare(DataEntry arg0, DataEntry arg1) {
 		if(arg0.getDate() > arg1.getDate())
-			return 1;
-		else if(arg0.getDate() < arg1.getDate())
 			return -1;
+		else if(arg0.getDate() < arg1.getDate())
+			return 1;
 		return 0;
 	}
 

7eb5259232806193aa742b231fb6f8687dfb48e5
Arno L. <arno.landuyt@hotmail.com>
2012-12-01 14:05:35 +0100

    Removed alive check message

--- src/main/java/me/arno/blocklog/schedules/AliveSchedule.java
+++ src/main/java/me/arno/blocklog/schedules/AliveSchedule.java
@@ -9,7 +9,6 @@ public class AliveSchedule implements Runnable {
 	
 	@Override
 	public void run() {
-		Util.sendNotice("Sending alive query");
 		try {
 			BlockLog.getInstance().getConnection().createStatement().executeUpdate("UPDATE blocklog_data SET `id`=1 WHERE `id`=1");
 		} catch(SQLException e) {
